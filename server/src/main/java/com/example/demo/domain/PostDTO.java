70e3071b5e6ec4c85456de4bdb189de95bbb8cc6
papamoon0113 <metube0113@gmail.com>
2023-10-07 16:36:05 +0900

    DTO 구축 예시 업로드

---
+++ server/src/main/java/com/example/demo/domain/PostDTO.java
@@ -0,0 +1,22 @@
+package com.example.demo.domain;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDTO {
+	private int postId;
+	private String content;
+	private String title;
+	private LocalDateTime postDate;
+	private String customerId;
+	private String postCategoryId;
+}
