1cf38929c1f11ac774fc034b5461c9fd6316e893
papamoon0113 <metube0113@gmail.com>
2023-10-07 10:32:16 +0900

    add post_review table

--- database/post_comment.sql
+++ database/post_comment.sql
@@ -1,9 +1,9 @@
-create table post_comment (
-   post_comment_id int primary key AUTO_INCREMENT,
-   customer_id varchar(15) not null,
-   post_id int not null,
-   content varchar(100) not null,
-   posted_date date not null,
+CREATE TABLE post_comment (
+   post_comment_id INT PRIMARY KEY AUTO_INCREMENT,
+   customer_id VARCHAR(15) NOT NULL,
+   post_id INT NOT NULL,
+   content VARCHAR(100) NOT NULL,
+   posted_date DATE NOT NULL,
    
    FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
    FOREIGN KEY(post_id) REFERENCES post(post_id)
