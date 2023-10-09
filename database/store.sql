a710c76e757829bfde416ffdf0f88997443f73b2
papamoon0113 <metube0113@gmail.com>
2023-10-09 09:21:55 +0900

    SQL 파일 수정

--- database/store.sql
+++ database/store.sql
@@ -2,7 +2,7 @@ create table store(
    store_id int primary key auto_increment,
    store_name varchar(20) not null,
    address varchar(40) not null,
-   phone_number varchar(12) not null,
+   phone_number varchar(20) not null,
    store_desc varchar(100) not null,
    latitude float not null, /* 위도 */
    longitude float not null, /* 경도 */
