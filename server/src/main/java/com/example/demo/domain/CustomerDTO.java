af79f72df25bff06447be52bfd107575577d191a
papamoon0113 <metube0113@gmail.com>
2023-10-09 09:44:55 +0900

    Post, PostComment, Customer DTO 생성

---
+++ server/src/main/java/com/example/demo/domain/CustomerDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+	private String customerId;
+	private String customerPassword;
+}
---
+++ server/src/main/java/com/example/demo/domain/PostCommentDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostCommentDTO {
+	private int postCommentId;
+	private String customerId;
+	private int postId;
+	private String content;
+	private LocalDateTime postedDate;
+}
