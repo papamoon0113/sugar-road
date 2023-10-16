710b419eeab214a4988c3ec30d2a569c8494ecad
papamoon0113 <metube0113@gmail.com>
2023-10-16 16:04:44 +0900

    Store DAO 수정

--- server/src/main/java/com/example/demo/dao/StoreDAO.java
+++ server/src/main/java/com/example/demo/dao/StoreDAO.java
@@ -8,23 +8,12 @@ import org.apache.ibatis.annotations.*;
 public interface StoreDAO {
     //R 다 불러 오는 거 // user_id 추가??
     @Select("select store_id, store_name, address, phone_number, store_desc, latitude, longitude  from store")
-    @Results({
-            @Result(property = "storeId", column = "store_id"),
-            @Result(property = "storeName", column = "store_name"),
-            @Result(property = "phoneNumber", column = "phone_number"),
-            @Result(property = "storeDesc", column = "store_desc"),
-    })
+
     public List<StoreDTO> readStore();
 
     // 선택한 게시물 불러오기
     @Select("select store_id, store_name, address, phone_number, store_desc, latitude, longitude, store_image_path  from store where store_id =#{storeId}")
-    @Results({
-            @Result(property = "storeId", column = "store_id"),
-            @Result(property = "storeName", column = "store_name"),
-            @Result(property = "phoneNumber", column = "phone_number"),
-            @Result(property = "storeDesc", column = "store_desc"),
-            @Result(property = "storeImagePath", column = "store_image_path")
-    })
+
     public StoreDTO readSelectStore(int storeId);
 
     //R 유동적 불러오기 (wherlt re절 컬럼명 = 값)
@@ -33,12 +22,7 @@ public interface StoreDAO {
 
     // 수정할 가게 내용 불러오기
     @Select("select store_id, store_name, address, phone_number, store_desc, latitude, longitude from store where store_id =#{storeId}")
-    @Results({
-            @Result(property = "storeId", column = "store_id"),
-            @Result(property = "storeName", column = "store_name"),
-            @Result(property = "phoneNumber", column = "phone_number"),
-            @Result(property = "storeDesc", column = "store_desc"),
-    })
+
     public StoreDTO ViewEditStore(int storeId);
 
     //U id 기준으로 가게 정보 수정 (가게이름, 주소, 전화번호, 가게상세설명, 위도, 경도, 사진)
