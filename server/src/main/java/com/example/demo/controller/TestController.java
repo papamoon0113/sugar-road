46899b0bea682852e265904f036031ca4edc31bf
papamoon0113 <metube0113@gmail.com>
2023-10-17 09:20:45 +0900

    이미지 컨트롤러 추가

--- server/src/main/java/com/example/demo/controller/TestController.java
+++ server/src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
