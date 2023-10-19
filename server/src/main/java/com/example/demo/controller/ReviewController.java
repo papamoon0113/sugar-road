774a1c5ad551dcb6f9660e03a9eaf451ca08ae99
papamoon0113 <metube0113@gmail.com>
2023-10-19 16:25:12 +0900

    리뷰 댓글 프론트 작업(AJAX)

--- server/src/main/java/com/example/demo/controller/ReviewController.java
+++ server/src/main/java/com/example/demo/controller/ReviewController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.dao.RecommendationDAO;
 import com.example.demo.dao.ReviewDAO;
 import com.example.demo.domain.ReviewDTO;
+import com.example.demo.domain.ReviewResultVO;
+import com.example.demo.util.ImageUtil;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,12 +23,14 @@ public class ReviewController {
 	ReviewDAO reviewDAO;
 
 	@Autowired
+	ImageUtil imageUtil;
+	@Autowired
 	RecommendationDAO recommendationDAO;
 
 	@GetMapping("/review")
 	@ResponseBody
-	public List<ReviewDTO> readReviewByStoreId(@RequestParam("storeId") int storeId) {
-		return reviewDAO.readReviewBy("store_id", String.valueOf(storeId));
+	public List<ReviewResultVO> readReviewByStoreId(@RequestParam("storeId") int storeId) {
+		return reviewDAO.readReviewOfStore(storeId);
 	}
 
 	@GetMapping("/review/detail")
@@ -37,12 +42,13 @@ public class ReviewController {
 	}
 
 	@PostMapping("/review/write")
-//	@ResponseBody
 	public String writeReview(ReviewDTO dto) {
 		System.out.println(dto.toString());
+		MultipartFile imageFile = dto.getUploadImage();
+		String reviewImagePath = imageUtil.writeImage(imageFile);
+		dto.setReviewImagePath(reviewImagePath);
 		reviewDAO.createReview(dto);
-//		return "redirect:/store/detail?storeId=" + dto.getStoreId();
-		return "redirect:/detail?storeId=" + dto.getStoreId();
+		return "redirect:/store/detail?storeId=" + dto.getStoreId();
 	}
 
 	@GetMapping("/review/write")
@@ -64,6 +70,10 @@ public class ReviewController {
 	@PostMapping("/review/edit")
 	@ResponseBody
 	public Boolean editReview(ReviewDTO dto) {
+		System.out.println(dto.toString());
+		MultipartFile imageFile = dto.getUploadImage();
+		String reviewImagePath = imageUtil.writeImage(imageFile);
+		dto.setReviewImagePath(reviewImagePath);
 		return reviewDAO.updateReview(dto);
 	}
 
--- server/src/main/java/com/example/demo/dao/RecommendationDAO.java
+++ server/src/main/java/com/example/demo/dao/RecommendationDAO.java
@@ -19,12 +19,12 @@ public interface RecommendationDAO {
     @Select("select recommendation_id, reference_type, reference_id, user_id, posted_date from recommendation")
     public List<RecommendationDTO> selectRecommendation();
 
-    @Select("select count(r1.recommendation_id) as 'recommendation_count', count(r2.recommendation_id) as 'recommendation_check'"
+    @Select("select count(r1.recommendation_id) as 'recommendation_count', count(r2.recommendation_id) as 'recommendation_check' "
         + "from (select recommendation_id "
                 + "from recommendation "
-                + "where reference_type = #{referenceType} and reference_id = #{referenceId}) as r1,"
-                + "(select recommendation_id"
-                + "from recommendation"
+                + "where reference_type = #{referenceType} and reference_id = #{referenceId}) as r1, "
+                + "(select recommendation_id "
+                + "from recommendation "
                 + "where reference_type = #{referenceType} and reference_id = #{referenceId} and user_id = #{userId}) as r2;")
     public RecommendationResultVO readRecommendation(RecommendationDTO dto);
 
@@ -34,7 +34,7 @@ public interface RecommendationDAO {
     @Delete("delete from recommendation where reference_type=#{referenceType} and reference_id = #{referenceId} and user_id = #{userId}")
     public boolean deleteRecommendation(RecommendationDTO dto);
 
-    @Select("select count(*) from recommendation where reference_type=#{referenceType} and reference_id = #{referenceId}")
+    @Select("select count(recommendation_id) from recommendation where reference_type=#{referenceType} and reference_id = #{referenceId}")
     public int readRecommendationCount(RecommendationDTO dto);
 
     @Select("select count(*) from recommendation where reference_type=#{referenceType} and reference_id = #{referenceId} and user_id = #{userId}")
