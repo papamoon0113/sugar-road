557d03a9ea4df12121b9b19a6a7e46776aa0cc96
undercover0072 <flotter586@gmail.com>
2023-10-20 18:26:16 +0900

    로그인, 회원가입, 탈퇴 css 적용 및 수정

--- server/src/main/java/com/example/demo/controller/UsersController.java
+++ server/src/main/java/com/example/demo/controller/UsersController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,34 +39,46 @@ public class UsersController {
 
     //회원가입
     @RequestMapping(value = "/users/signup", method = RequestMethod.POST)
-    @ResponseBody
-    public String createUsers(UsersDTO newbieDTO){
+    public String createUsers(UsersDTO newbieDTO,
+                              Model model){
 
         MultipartFile imageFile = newbieDTO.getImage();
-        String userImagePath = imageUtil.writeImage(imageFile);
-        newbieDTO.setUserImagePath(userImagePath);
 
-        if(usersDAO.createUser(newbieDTO)){ //회원 생성 성공 시
-            return "회원가입에 성공했습니다";
-        } else if(!(usersDAO.createUser(newbieDTO))){
-            return "회원가입에 실패했습니다";
-        } else {
-            return "오류가 발생했습니다";
+        if(!(imageFile.isEmpty())) { //입력된 파일이 없지 않으면 이미지 삽입 실행
+            String userImagePath = imageUtil.writeImage(imageFile);
+            newbieDTO.setUserImagePath(userImagePath);
         }
+
+        //입력한 아이디 중복 확인
+        List<UsersDTO> searchId = usersDAO.readUserBy("user_id", newbieDTO.getUserId());
+        if(searchId.isEmpty()){ //중복된 아이디 조회 결과가 없으면
+            if(usersDAO.createUser(newbieDTO)) { //회원 생성
+                System.out.println("회원가입에 성공했습니다");
+                return "redirect:/users/login";
+            }
+        } else {
+            System.out.println("입력된 아이디와 중복되는 아이디가 있습니다");
+            model.addAttribute("msg", "중복되는 아이디가 존재합니다");
+
+            return "/users/signup";
+        }
+        System.out.println("오류가 발생했습니다");
+        return "redirect:/users/signup";
     }
 
     //회원 로그인
     @RequestMapping(value = "/users/login", method = RequestMethod.POST)
-    @ResponseBody
     public String loginUsers(UsersDTO unknownDTO,
-                                   HttpServletRequest servletRequest){
-        ModelAndView mav = new ModelAndView();
+                           HttpServletRequest servletRequest,
+                           Model model){
         List<UsersDTO> selectIdResult = usersDAO.readUserBy("user_id", unknownDTO.getUserId()); //입력한 아이디 검색 결과
 
         if(selectIdResult.isEmpty()){
             System.out.println("존재하지 않는 아이디입니다");
-            return "존재하지 않는 아이디입니다";
-        } else {
+            //session.setAttribute("statusMsg", "존재하지 않는 아이디입니다");
+            model.addAttribute("msg", "존재하지 않는 아이디입니다");
+            return "/users/login";
+        }
 
             String selectIdPwd = (selectIdResult.get(0)).getUserPassword(); //조회된 아이디의 패스워드
 
@@ -73,24 +86,25 @@ public class UsersController {
 
             if (selectIdPwd.compareTo(unknownDTO.getUserPassword()) != 0) { //입력한 패스워드와 계정의 패스워드가 일치하지 않으면
                 System.out.println("패스워드가 일치하지 않습니다");
-                return "패스워드가 일치하지 않습니다";
+                //session.setAttribute("statusMsg", "패스워드가 일치하지 않습니다");
+                model.addAttribute("msg", "패스워드가 일치하지 않습니다");
+                return "/users/login";
+            } else {
+
+                System.out.println("로그인에 성공했습니다");
+
+                //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+                HttpSession session = servletRequest.getSession();
+
+                //로그인 성공한 유저의 ID 정보를 값으로 갖는 "nowLogin" 세션 생성
+                session.setAttribute("nowLogin", selectIdResult.get(0).getUserId());
+                //session.setAttribute("statusMsg", "로그인에 성공하였습니다");
+                session.setAttribute("status", "loging");
+
+                //로그인에 성공하면 마이페이지로 이동
+                //추후 홈.html으로 수정예정
+                return "redirect:/mypage";
             }
-
-            System.out.println("로그인에 성공했습니다");
-
-            //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
-            HttpSession session = servletRequest.getSession();
-
-            //로그인 성공한 유저의 ID 정보를 값으로 갖는 "nowLogin" 세션 생성
-            session.setAttribute("nowLogin", selectIdResult.get(0).getUserId());
-
-            //로그인에 성공하면 마이페이지로 이동
-            //추후 홈.html으로 수정예정
-            //servletRequest.setAttribute("msg", "로그인에 성공하였습니다");
-            //servletRequest.setAttribute("url", "/mypage");
-            return "redirect:/mypage";
-        }
-
     }
 
 }
